de9721d8e49139250b54515c5ff9c6b34eb60d7f
Shiraishi <m1m24a20@oit.ac.jp>
2024-07-29 03:55:59 +0900

    課題1と課題2

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -1,7 +1,59 @@
 package org.example;
 
 public class Calculate {
+
   public int sum(int x, int y) {
     return x + y;
+  }
+
+  public int sigma(int x, int y) {
+    int total = 0;
+
+    for (int i = x; i <= y; i++) {
+      total += i;
+    }
+
+    return total;
+  }
+
+  public int oddsigma(int x, int y) {
+    int total = 0;
+
+    for (int i = x; i <= y; i++) {
+      if (i % 2 != 0) {
+        total += i;
+      }
+    }
+
+    return total;
+  }
+
+  public int evensigma(int x, int y) {
+    int total = 0;
+
+    for (int i = x; i <= y; i++) {
+      if (i % 2 == 0) {
+        total += i;
+      }
+    }
+
+    return total;
+  }
+
+  public static void main(String[] args) {
+    Calculate c = new Calculate();
+
+    System.out.println("Hello World!");
+
+    int a = 2;
+    int b = 3;
+    System.out.println("Sum of " + a + "and " + b + " is " + c.sum(a, b) + ". Average is " + c.sum(a, b) / 2 + ".");
+
+    a = 1;
+    b = 10;
+    System.out.println("Sum of " + a + "to " + b + " is " + c.sigma(a, b) + ". Average is " + c.sigma(a, b) / 10 + ".");
+
+    System.out.println(
+        "Sum of odd of " + a + "to " + b + " is " + c.oddsigma(a, b) + ". Sum of even is " + c.evensigma(a, b) + ".");
   }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -11,4 +11,24 @@ public class CalculateTest {
     assertEquals(expected, calculate.sum(2, 3));
   }
 
+  @Test
+  public void testSigma() {
+    Calculate calculate = new Calculate();
+    int expected = 55;
+    assertEquals(expected, calculate.sigma(1, 10));
+  }
+
+  @Test
+  public void testOddsigma() {
+    Calculate calculate = new Calculate();
+    int expected = 25;
+    assertEquals(expected, calculate.oddsigma(1, 10));
+  }
+
+  @Test
+  public void testEvensigma() {
+    Calculate calculate = new Calculate();
+    int expected = 30;
+    assertEquals(expected, calculate.evensigma(1, 10));
+  }
 }
